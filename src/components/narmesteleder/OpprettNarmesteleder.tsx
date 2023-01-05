e160ba1ec16ed93b93ab64d8f249de30e5a2420c
Tia Firing <tia.firing@nav.no>
2023-01-05 14:52:31 +0100

    wip: bytter ut datepicker

--- src/components/narmesteleder/OpprettNarmesteleder.tsx
+++ src/components/narmesteleder/OpprettNarmesteleder.tsx
@@ -1,10 +1,11 @@
 import { Alert, Button, Checkbox, Heading, TextField } from '@navikt/ds-react';
 import { useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import { Datepicker } from '@navikt/ds-datepicker';
+import { FormProvider, useForm } from 'react-hook-form';
 import { format } from 'date-fns';
 
-interface FormValues {
+import AktivFraOgMed from './AktivFraOgMed';
+
+export interface NarmestelederFormValues {
     ansattFnr: string;
     lederFnr: string;
     orgnummer: string;
@@ -16,12 +17,7 @@ interface FormValues {
 
 function OpprettNarmesteleder(): JSX.Element {
     const dagensDato = format(new Date(), 'yyyy-MM-dd');
-    const {
-        register,
-        control,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<FormValues>({
+    const form = useForm<NarmestelederFormValues>({
         defaultValues: {
             aktivFom: dagensDato,
         },
@@ -30,7 +26,7 @@ function OpprettNarmesteleder(): JSX.Element {
     const [result, setResult] = useState<string | null>(null);
     const OPPRETT_NL_URL = `/api/proxy/narmesteleder/opprett`;
 
-    const postData = async (data: FormValues): Promise<void> => {
+    const postData = async (data: NarmestelederFormValues): Promise<void> => {
         setError(null);
         setResult(null);
         const response = await fetch(OPPRETT_NL_URL, {
@@ -46,51 +42,46 @@ function OpprettNarmesteleder(): JSX.Element {
     };
 
     return (
-        <form onSubmit={handleSubmit(postData)}>
-            <Heading size="medium" level="2">
-                Registrer nærmeste leder
-            </Heading>
-            <p />
-            <TextField
-                {...register('ansattFnr', { required: true })}
-                label="Fødselsnummer"
-                error={errors.ansattFnr && 'Fødselsnummer for den sykmeldte mangler'}
-            />
-            <TextField
-                {...register('lederFnr', { required: true })}
-                label="Fødselsnummer til ny nærmeste leder"
-                error={errors.lederFnr && 'Fødselsnummer for nærmeste leder mangler'}
-            />
-            <TextField
-                {...register('orgnummer', { required: true })}
-                label="Organisasjonsnummer"
-                error={errors.orgnummer && 'Organisasjonsnummer mangler'}
-            />
-            <TextField
-                {...register('mobil', { required: true })}
-                label="Telefonnummer til ny nærmeste leder"
-                error={errors.mobil && 'Telefonnummer for nærmeste leder mangler'}
-            />
-            <TextField
-                {...register('epost', { required: true })}
-                label="E-post til ny nærmeste leder"
-                error={errors.epost && 'E-post for nærmeste leder mangler'}
-            />
-            <Checkbox {...register('forskutterer')}>Arbeidsgiver forskutterer</Checkbox>
-            <p>
-                <b>Aktiv fra og med</b>
-            </p>
-            <Controller
-                control={control}
-                name="aktivFom"
-                render={({ field }) => <Datepicker onChange={(date) => field.onChange(date)} value={field.value} />}
-            />
-            <p />
-            <Button type="submit">Registrer</Button>
-            <p />
-            {error && <Alert variant="error">{error}</Alert>}
-            {result && <Alert variant="success">{result}</Alert>}
-        </form>
+        <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(postData)}>
+                <Heading size="medium" level="2">
+                    Registrer nærmeste leder
+                </Heading>
+                <p />
+                <TextField
+                    {...form.register('ansattFnr', { required: true })}
+                    label="Fødselsnummer"
+                    error={form.formState.errors.ansattFnr && 'Fødselsnummer for den sykmeldte mangler'}
+                />
+                <TextField
+                    {...form.register('lederFnr', { required: true })}
+                    label="Fødselsnummer til ny nærmeste leder"
+                    error={form.formState.errors.lederFnr && 'Fødselsnummer for nærmeste leder mangler'}
+                />
+                <TextField
+                    {...form.register('orgnummer', { required: true })}
+                    label="Organisasjonsnummer"
+                    error={form.formState.errors.orgnummer && 'Organisasjonsnummer mangler'}
+                />
+                <TextField
+                    {...form.register('mobil', { required: true })}
+                    label="Telefonnummer til ny nærmeste leder"
+                    error={form.formState.errors.mobil && 'Telefonnummer for nærmeste leder mangler'}
+                />
+                <TextField
+                    {...form.register('epost', { required: true })}
+                    label="E-post til ny nærmeste leder"
+                    error={form.formState.errors.epost && 'E-post for nærmeste leder mangler'}
+                />
+                <Checkbox {...form.register('forskutterer')}>Arbeidsgiver forskutterer</Checkbox>
+                <AktivFraOgMed />
+                <p />
+                <Button type="submit">Registrer</Button>
+                <p />
+                {error && <Alert variant="error">{error}</Alert>}
+                {result && <Alert variant="success">{result}</Alert>}
+            </form>
+        </FormProvider>
     );
 }
 
