cc6859d20d11f826a04077a2bfb1464cfc0576a5
Karl J. Overå <k@rl.run>
2023-08-08 16:30:45 +0200

    add back root text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 import { ReactElement } from 'react'
+import { Metadata } from 'next'
+
+import { BodyShort } from '../components/aksel/server'
+
+export const metadata: Metadata = {
+    title: 'Team Sykmelding Mock',
+}
 
 function Page(): ReactElement {
-    return <div>hey</div>
+    return (
+        <div>
+            <BodyShort spacing>Velkommen til Team Sykmelding sin mock!</BodyShort>
+            <BodyShort>Velg hva du vil gjøre i side menyen!</BodyShort>
+        </div>
+    )
 }
 
 export default Page
