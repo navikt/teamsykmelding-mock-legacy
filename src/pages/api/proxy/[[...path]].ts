300a566856374ad9dc2ac0cfdb4032722be6525a
Karl O <k@rl.run>
2023-08-08 11:14:22 +0200

    use @navikt/next-api-proxy (#30)

---
+++ src/pages/api/proxy/[[...path]].ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { proxyApiRouteRequest } from '@navikt/next-api-proxy'
+
+import { getEnv } from '../../../utils/env'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    if (process.env.NODE_ENV !== 'production') {
+        res.status(200).json({ message: 'Jobber lokalt, 200 ok læll' })
+        return
+    }
+
+    const rewrittenPath = req.url!.replace(`/api/proxy`, '')
+    await proxyApiRouteRequest({
+        path: rewrittenPath,
+        req,
+        res,
+        hostname: getEnv('MOCK_BACKEND_URL'),
+        https: false,
+    })
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+        externalResolver: true,
+    },
+}
+
+export default handler
