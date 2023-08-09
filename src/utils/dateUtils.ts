36d42c5b5f3dc0e9b951ff079999f6cd0200d0e9
Karl J. Overå <k@rl.run>
2023-08-09 11:33:18 +0200

    add some 'presets' when creating sykmeldinger

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,5 +1,9 @@
-import { parseISO } from 'date-fns'
+import { formatISO, parseISO, sub } from 'date-fns'
 
 export function toDate(date: string | Date): Date {
     return typeof date === 'string' ? parseISO(date) : date
+}
+
+export function subDays(date: string | Date, days: number): string {
+    return formatISO(sub(toDate(date), { days }))
 }
