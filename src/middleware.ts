dbdb649aa7a90d8065edc6cdf8415ea057cfa14e
Karl J. Overå <k@rl.run>
2024-05-29 08:12:12 +0200

    chore: redirect from old to new ingress

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { logger } from '@navikt/next-logger'
+
+export function middleware(request: NextRequest): NextResponse | void {
+    const url = new URL(request.url)
+    const forwardedHostHeader = request.headers.get('x-forwarded-host')
+
+    // Redirect to ingress
+    if (forwardedHostHeader?.includes('intern')) {
+        logger.info('Hit old ingress, redirecting to new ingress')
+        return NextResponse.redirect(new URL(url.pathname, 'https://teamsykmelding-mock.ansatt.dev.nav.no'))
+    }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
